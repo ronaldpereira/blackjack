f17a686af80639a9a4626ee89842421d137fa301
Ronald Pereira <ronald.drp11@gmail.com>
2017-05-06 10:32:50 -0300

    Added comments

--- src/code/CriaBaralho.java
+++ src/code/CriaBaralho.java
@@ -54,12 +54,12 @@ public class CriaBaralho
         }
 
         Random rng = new Random();
-        int aux = rng.nextInt(10000);
+        int aux = rng.nextInt(10000); // Gera um inteiro randomico entre 0 e 10000
         System.out.println("RNG = "+aux); // TODO print do RNG teste
 
         for (int i = 0; i < aux; i++)
         {
-            Collections.shuffle(baralho);
+            Collections.shuffle(baralho); // Embaralha randomicamente o baralho
         }
 
         System.out.println("Topo do baralho é "+baralho.get(0).retornaNomeeNaipe()); // TODO print de nome e naipe do topo teste
