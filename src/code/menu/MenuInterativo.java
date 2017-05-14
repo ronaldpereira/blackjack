d954487031fd17a577f30df4aed82d7dbe856724
Ronald Pereira <ronald.drp11@gmail.com>
2017-05-14 17:15:34 -0300

    Updated MenuInterativo class and comments

--- src/code/menu/MenuInterativo.java
+++ src/code/menu/MenuInterativo.java
@@ -3,19 +3,29 @@ package menu;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class MenuInterativo
+public class MenuInterativo // Classe que representa o menu interativo do jogo
 {
     private jogador.Jogador[] jogador; // Vetor de jogadores
     private int numJogadores = 0; // Guarda o numero de jogadores
     private BufferedReader reader; // Leitor da entrada
 
-    public void apresentacao()
+    public jogador.Jogador[] executaMenu() // Metodo que executa o MenuInterativo e retorna um vetor do tipo Jogador do pacote jogador
+    throws Exception
+    {
+        this.apresentacao();
+        this.pegaNumJogadores();
+
+        return jogador;
+    }
+
+    public void apresentacao() // Metodo que realiza a apresentacao do jogo
     {
         System.out.println("Bem-vindo ao JackBlack 2017/1");
         System.out.println("Autores: Marina Monteiro Moreira e Ronald Davi Rodrigues Pereira");
     }
 
-    public void pegaNumJogadores() throws Exception
+    public void pegaNumJogadores() // Metodo que realiza a leitura de quantos jogadores vao jogar o jogo
+    throws Exception
     {
         while(numJogadores > 6 || numJogadores <= 0)
         {
@@ -24,11 +34,12 @@ public class MenuInterativo
             numJogadores = Integer.parseInt(reader.readLine());
         }
 
-        this.criaJogadores(numJogadores);
-        this.saldoInicial(numJogadores);
+        criaJogadores(numJogadores);
+        saldoInicial(numJogadores);
     }
 
-    public void criaJogadores(int id) throws Exception
+    private void criaJogadores(int id) // Metodo que realiza a criacao dos objetos dos jogadores, utilizando a classe Jogador do pacote jogador
+    throws Exception
     {
         jogador = new jogador.Jogador[numJogadores+1];
         for(int i = 1; i <= numJogadores; i++)
@@ -38,7 +49,8 @@ public class MenuInterativo
         }
     }
 
-    public void saldoInicial(int numJogadores) throws Exception
+    private void saldoInicial(int numJogadores) // Metodo que realiza a leitura do saldo inicial dos jogadores e coloca para cada jogador esse saldo
+    throws Exception
     {
         double valor = 0;
         while(valor < 10)
@@ -52,14 +64,6 @@ public class MenuInterativo
         {
             this.jogador[i].aumentaSaldo(valor);
         }
-    }
-
-    public jogador.Jogador[] executaMenu() throws Exception
-    {
-        this.apresentacao();
-        this.pegaNumJogadores();
-
-        return jogador;
     }
 
     public int retornaNumJogadores()
