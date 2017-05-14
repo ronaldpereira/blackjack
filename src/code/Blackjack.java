f2062729a22c9b484742914c3d23249e14fa5069
Ronald Pereira <ronald.drp11@gmail.com>
2017-05-14 16:57:54 -0300

    Updated Blackjack class

--- src/code/Blackjack.java
+++ src/code/Blackjack.java
@@ -5,7 +5,7 @@ public class Blackjack // Classe principal do jogo
 {
     public static void main(String[] args) throws Exception
     {
-        jogo.JogaJogo jogajogo = new jogo.JogaJogo(); // Cria um novo objeto do tipo JogaJogo, que está no pacote jogo
-        jogajogo.jogo(); // Chama 
+        jogo.JogaJogo jogajogo = new jogo.JogaJogo(); // Cria um novo objeto do tipo JogaJogo, que esta no pacote jogo
+        jogajogo.jogo(); // Chama o metodo que executa o jogo
     }
 }
